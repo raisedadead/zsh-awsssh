ee7d2c87a59a9a045ac53996845c1e12ed37de35
Mrugesh Mohapatra <noreply@mrugesh.dev>
2024-03-22 01:21:50 +0530

    fix: whitespace

--- awsssh.plugin.zsh
+++ awsssh.plugin.zsh
@@ -3,7 +3,7 @@
 
 # Check for required commands
 for cmd in bat awk fzf aws rg; do
-  if ! command -v $cmd &> /dev/null; then
+  if ! command -v $cmd &>/dev/null; then
     echo "Missing required tool: $cmd"
     exit 1
   fi
@@ -29,13 +29,11 @@ _aws_query_for_instances() {
     --region "$region" \
     --filters Name=tag:$tag_key,Values="$tag_value" \
     --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`].Value | [0], InstanceId, PrivateIpAddress, PublicIpAddress, State.Name, ImageId, InstanceType, PublicDnsName]' \
-    --output text | \
-    rg -v '^None$' | \
+    --output text |
+    rg -v '^None$' |
     # Ensure awk prints fields in the correct order
     awk '{printf "%-30s\t%-20s\t%-15s\t%-15s\t%-15s\t%-20s\t%-10s\t%s\n", $1, $2, $3, $4, $5, $6, $7, $8}'
 }
-
-
 
 _aws_ssh_main() {
 
@@ -62,47 +60,46 @@ _aws_ssh_main() {
 
   local selection=$(
     _aws_query_for_instances "$region" "$tag_key" "$tag_value" |
-    fzf \
-    --height=40% \
-    --layout=reverse \
-    --border \
-    --border-label="EC2 Instances" \
-    --info=default \
-    --prompt="Search Instance: " \
-    --header="Select (Enter), Toggle Details (Ctrl-/), Quit (Ctrl-C or ESC)" \
-    --header-lines=1 \
-    --bind="ctrl-/:toggle-preview" \
-    --preview-window="right:40%:wrap" \
-    --preview-label="Details" \
-    --preview='
+      fzf \
+        --height=40% \
+        --layout=reverse \
+        --border \
+        --border-label="EC2 Instances" \
+        --info=default \
+        --prompt="Search Instance: " \
+        --header="Select (Enter), Toggle Details (Ctrl-/), Quit (Ctrl-C or ESC)" \
+        --header-lines=1 \
+        --bind="ctrl-/:toggle-preview" \
+        --preview-window="right:40%:wrap" \
+        --preview-label="Details" \
+        --preview='
         echo {} |
         awk -F"\t" "{
           print \"Name: \" \$1 \"\\nInstance ID: \" \$2 \"\\nPrivate IP: \" \$3 \"\\nPublic IP: \" \$4 \"\\nStatus: \" \$5 \"\\nAMI: \" \$6 \"\\nType: \" \$7 \"\\nPublic DNS Name: \" \$8
         }"
       ' \
-    --delimiter=$'\t' \
-    --with-nth=1,2,3,4,5
+        --delimiter=$'\t' \
+        --with-nth=1,2,3,4,5
   )
-
 
   local public_dns=$(echo $selection | awk '{print $8}')
   local instance_status=$(echo $selection | awk '{print $5}')
 
   if [ "$instance_status" != "running" ]; then
-      echo ""
-      echo "Aborting. Instance is $instance_status."
+    echo ""
+    echo "Aborting. Instance is $instance_status."
   elif [ -n "$public_dns" ]; then
-      local default_username="ec2-user"
-      local username_prompt="Enter username [$default_username]: "
-      echo -n "$username_prompt"
-      read username
-      username=${username:-$default_username}
-      echo ""
-      echo "Connecting to $public_dns..."
-      ssh "$username@$public_dns"
+    local default_username="ec2-user"
+    local username_prompt="Enter username [$default_username]: "
+    echo -n "$username_prompt"
+    read username
+    username=${username:-$default_username}
+    echo ""
+    echo "Connecting to $public_dns..."
+    ssh "$username@$public_dns"
   else
-      echo ""
-      echo "No instance selected."
+    echo ""
+    echo "No instance selected."
   fi
 }
 
